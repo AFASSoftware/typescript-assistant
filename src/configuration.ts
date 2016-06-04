cbd948941e0e7321541154bb1bc3f2101b7d0faf
Johan Gorter <drsoong@gmail.com>
2016-06-04 20:24:37 +0000

    documentation

--- src/configuration.ts
+++ src/configuration.ts
@@ -17,6 +17,10 @@ export let createConfiguration = () => {
   /* tslint:disable:no-require-imports */
   let tsConfig = JSON.parse(readFileSync(join(process.cwd(), './tsconfig.json'), 'UTF-8'));
   // let tsConfig = require('./tsconfig.json');
+  
+  if (!tsConfig.compilerOptions.outDir) {
+    throw new Error('No outdir found in tsconfig.json, please add "outDir": "./build/js" to the compilerOptions');
+  }
 
   let compiledTestFolder = join(tsConfig.compilerOptions.outDir, tsaConfig.testDir);
   return {
