dfb37a7b53f5651f403a821d57ba9e936de35bf3
Johan Gorter <jgo@AFASs-MacBook-Pro.local>
2017-08-02 21:40:22 +0200

    preventing crazy \r character

---
+++ test/index-tests.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import * as fs from 'fs';
+
+describe('index', () => {
+
+  it('contains a first line that allows it to be executed on every platform', () => {
+    let contents = fs.readFileSync(__dirname + '/../src/index.ts', 'utf-8');
+    let lines = contents.split('\n');
+    expect(lines[0]).to.equal('#!/usr/bin/env node'); // important: no \r at the end
+  });
+
+  it('compiled code contains a first line that allows it to be executed on every platform', () => {
+    let builtFile = __dirname + '/../build/js/src/index.js';
+    if (!fs.existsSync(builtFile)) {
+      return;
+    }
+    let contents = fs.readFileSync(builtFile, 'utf-8');
+    let lines = contents.split('\n');
+    expect(lines[0]).to.equal('#!/usr/bin/env node'); // important: no \r at the end
+  });
+
+});
