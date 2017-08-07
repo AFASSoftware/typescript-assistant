76075ce92f63c44f4e6f4eda9feceeebecd9ea52
Johan Gorter <johan-gorter@users.noreply.github.com>
2017-08-07 15:48:33 +0200

    last tweaks

---
+++ src/dependency-injector.ts
@@ -0,0 +1,34 @@
+import { Dependencies } from './dependencies';
+import { createWatcher } from './watcher';
+import { createNyc } from './testing/nyc';
+import { createLinter } from './code-style/linter';
+import { createFormatter } from './code-style/formatter';
+import { createGit } from './git';
+import { createCompiler } from './compiler';
+import { createInjector } from './injector';
+import { createBus } from './bus';
+import { createDefaultTaskRunner, createWindowsTaskRunner } from './taskrunner';
+import { sep } from 'path';
+import { createConsoleLogger } from './logger';
+
+export let createDependencyInjector = (): <T>(createFunction: (dependencies: Partial<Dependencies>) => T) => T => {
+  let logger = createConsoleLogger();
+  let taskRunner = sep === '\\' ? createWindowsTaskRunner() : createDefaultTaskRunner();
+  let bus = createBus();
+
+  let dependencies: Partial<Dependencies> = {
+    bus, logger, taskRunner
+  };
+
+  let { inject } = createInjector(dependencies);
+
+  dependencies.inject = inject;
+  dependencies.compiler = inject(createCompiler);
+  dependencies.git = inject(createGit);
+  dependencies.formatter = inject(createFormatter);
+  dependencies.linter = inject(createLinter);
+  dependencies.nyc = inject(createNyc);
+  dependencies.watcher = inject(createWatcher);
+
+  return dependencies.inject;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,17 @@
 #!/usr/bin/env node
 
-import { createFormatter } from './code-style/formatter';
-import { createLinter } from './code-style/linter';
-import { createCompiler } from './compiler';
-import { createGit } from './git';
-import { createConsoleLogger } from './logger';
-import { createDefaultTaskRunner, createWindowsTaskRunner } from './taskrunner';
-import { Dependencies } from './dependencies';
-
-import { sep } from 'path';
-import { createInjector } from './injector';
 import { createFixCommand } from './commands/format';
 import { createCleanCommand } from './commands/clean';
 import { createReleaseCommand } from './commands/release';
 import { createAssistCommand } from './commands/assist';
-import { createNyc } from './testing/nyc';
-import { createBus } from './bus';
 import { createPostCheckoutCommand } from './commands/post-checkout';
 import { createPostMergeCommand } from './commands/post-merge';
 import { createPreCommitCommand } from './commands/pre-commit';
-import { createWatcher } from './watcher';
 import { createCICommand } from './commands/ci';
 import * as yargsModule from 'yargs';
+import { createDependencyInjector } from './dependency-injector';
 
-let logger = createConsoleLogger();
-let taskRunner = sep === '\\' ? createWindowsTaskRunner() : createDefaultTaskRunner();
-let bus = createBus();
-
-let dependencies: Partial<Dependencies> = {
-  bus, logger, taskRunner
-};
-let { inject } = createInjector(dependencies);
-
-dependencies.inject = inject;
-dependencies.compiler = inject(createCompiler);
-dependencies.git = inject(createGit);
-dependencies.formatter = inject(createFormatter);
-dependencies.linter = inject(createLinter);
-dependencies.nyc = inject(createNyc);
-dependencies.watcher = inject(createWatcher);
+let inject = createDependencyInjector();
 
 /* tslint:disable:no-console */
 let success = () => {
@@ -68,14 +41,14 @@ yargsModule.command(['clean', 'c'], 'Deletes all output files and intermediate f
 });
 
 yargsModule.command(['release'], 'Interactively makes a new version number, tags, pushes and publishes to npm', {}, (yargs) => {
-  inject(createReleaseCommand).execute();
+  inject(createReleaseCommand).execute().then(success, failure);
 });
 
 yargsModule.command(['ci'], 'Runs all tools in parallel to find errors', {
   '--': { describe: 'Arguments to be passed to tsc' }
 }, (yargs) => {
   let tscArgs = yargs._.slice(1);
-  inject(createCICommand).execute({ tscArgs });
+  inject(createCICommand).execute({ tscArgs }).then(success, failure);
 });
 
 yargsModule.command('pre-commit', 'Pre-commit git hook for husky', {}, (yargs) => {
@@ -89,12 +62,6 @@ yargsModule.command('post-checkout', 'Post-checkout git hook for husky', {}, (ya
 yargsModule.command('post-merge', 'Post-merge git hook for husky', {}, (yargs) => {
   inject(createPostMergeCommand).execute();
 });
-
-// if (!argsOk) {
-//   console.error('Usage: tsa || tsa f[ix] || tsa release || tsa clean || tsa ci');
-//   console.error('  || tsa pre-commit || tsa post-checkout || tsa post-merge');
-//   process.exit(1);
-// }
 
 yargsModule.help().strict().argv;
 
