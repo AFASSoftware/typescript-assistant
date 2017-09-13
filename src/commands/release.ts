0cb548ac02ecbf68b0c5e2ced75b703f9bd00ab1
Johan Gorter <jgo@AFASs-MacBook-Pro.local>
2017-09-13 22:26:25 +0200

    added a pre-release publish feature

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -11,24 +11,37 @@ export let createReleaseCommand = (deps: Dependencies) => {
       if (!pristine) {
         throw new Error('There are uncommitted changes in the working tree');
       }
-
       let npm = sep === '\\' ? 'npm.cmd' : 'npm';
-      let answers = await prompt({
-        type: 'list',
-        name: 'bump',
-        message: 'What type of bump would you like to do?',
-        choices: ['patch', 'minor', 'major']
-      });
-      let importance = answers['bump'] as string;
 
-      // 'npm version' also does a 'git commit' and 'git tag'
-      await taskRunner.runTask(npm, ['version', importance], { name: 'npm', logger }).result;
+      let onBranch = await git.isOnBranch();
+      if (onBranch) {
+        let answers = await prompt({
+          type: 'confirm',
+          name: 'confirm',
+          message: 'You are not on master, do you want to do a pre-release?'
+        });
+        if (!answers[0]) {
+          return;
+        }
+        await taskRunner.runTask(npm, ['version', 'prerelease'], { name: 'npm', logger }).result;
+      } else {
+        let answers = await prompt({
+          type: 'list',
+          name: 'bump',
+          message: 'What type of bump would you like to do?',
+          choices: ['patch', 'minor', 'major']
+        });
+        let importance = answers['bump'] as string;
+
+        // 'npm version' also does a 'git commit' and 'git tag'
+        await taskRunner.runTask(npm, ['version', importance], { name: 'npm', logger }).result;
+      }
 
       await git.execute(['push', '--no-verify']);
-
       await git.execute(['push', '--tags', '--no-verify']);
 
-      await taskRunner.runTask(npm, ['publish'], { name: 'npm', logger }).result;
+      let publishArguments = onBranch ? ['publish', '--tag', 'dev'] : ['publish'];
+      await taskRunner.runTask(npm, publishArguments, { name: 'npm', logger }).result;
     }
   };
 };
