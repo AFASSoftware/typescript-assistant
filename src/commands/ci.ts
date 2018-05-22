6ec69e5d841944ff168d9ca941d3393f2a81c190
Joeri van Oostveen <j.vanoostveen@afas.nl>
2018-05-22 15:16:15 +0200

    Added debugging information.

--- src/commands/ci.ts
+++ src/commands/ci.ts
@@ -1,5 +1,13 @@
 import { Dependencies } from '../dependencies';
 
+let detectSocketError = (deps: Dependencies, location: string) => (err: Error) => {
+  if (err.hasOwnProperty('message') && err.message === 'socket hang up') {
+    deps.logger.log('Socket error at', location);
+  }
+
+  return err;
+};
+
 export let createCICommand = (deps: Dependencies) => {
   let { formatter, linter, compiler, nyc, git, logger } = deps;
 
@@ -8,10 +16,10 @@ export let createCICommand = (deps: Dependencies) => {
       let timestamp = new Date().getTime();
       let allTypescriptFiles = await git.findAllTypescriptFiles();
       let results = await Promise.all([
-        compiler.runOnce([]),
-        formatter.verifyFiles(allTypescriptFiles),
-        linter.lintOnce(false, allTypescriptFiles),
-        nyc.run()
+        compiler.runOnce([]).catch(detectSocketError(deps, 'Compiler')),
+        formatter.verifyFiles(allTypescriptFiles).catch(detectSocketError(deps, 'Formatter')),
+        linter.lintOnce(false, allTypescriptFiles).catch(detectSocketError(deps, 'Linter')),
+        nyc.run().catch(detectSocketError(deps, 'NYC'))
       ]);
       let toolErrors = results.filter(result => result === false).length;
       logger.log('ci', `CI tasks took ${Math.round((new Date().getTime() - timestamp) / 1000)} seconds`);
