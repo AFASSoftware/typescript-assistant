e0d486a1d41e4742b08cdbc00d84b329b3c78249
Johan Gorter <johan-gorter@users.noreply.github.com>
2024-06-17 10:35:32 +0200

    16gb memory for linting

--- src/code-style/linter.ts
+++ src/code-style/linter.ts
@@ -85,7 +85,7 @@ export function createLinter(dependencies: {
   }
 
   function startProcess() {
-    let env = Object.assign({ "NODE_OPTIONS" : "--max-old-space-size=8192" }, process.env );
+    let env = Object.assign({ "NODE_OPTIONS" : "--max-old-space-size=16384" }, process.env );
     lintProcess = fork(`${__dirname}/linter-process-eslint`, [], {
       execArgv: process.execArgv.filter((arg) => !arg.includes("inspect")),
       env
